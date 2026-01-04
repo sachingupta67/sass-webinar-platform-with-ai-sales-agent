1970139792b69f9a5d3f13635f11405c0a2a2db6
Sachin Gupta <sachin.gupta@peason.com>
2026-01-04 12:54:19 +0530

    Basic | Clerk Setup done

---
+++ app/(auth)/callback/page.tsx
@@ -0,0 +1,9 @@
+// TODO : Implement this page to handle the callback from the auth provider
+
+export const dynamic = "force-dynamic"; // Render fresh on every request because by default next js serves is static or cached
+
+const AuthCallbackPage = () => {
+  return <div>AuthCallbackPage</div>;
+};
+
+export default AuthCallbackPage;
