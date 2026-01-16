9a010a136001ffb2374322aca36f2ea2f3d06490
Sachin Gupta <sachin.gupta@peason.com>
2026-01-16 16:15:43 +0530

    Webinar Upcoming state

--- app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/RenderWebinar.tsx
+++ app/(publicRoutes)/live-webinar/[liveWebinarId]/_components/RenderWebinar.tsx
@@ -1,10 +1,12 @@
-import {
-  User,
-  Webinar,
-  WebinarStatusEnum,
-} from "@/lib/generated/prisma/client";
-import React from "react";
+"use client";
+
+import type { User, Webinar } from "@/lib/generated/prisma/client";
+import { WebinarStatusEnum } from "@/lib/generated/prisma/enums";
+import React, { useEffect } from "react";
 import WebinarUpcomingState from "./UpcomingWebinar/WebinarUpcomingState";
+import { usePathname, useRouter } from "next/navigation";
+import { useAttendeeStore } from "@/store/useAttendeeStore";
+import { toast } from "sonner";
 
 type Props = {
   apiKey: string;
@@ -15,7 +17,17 @@ type Props = {
   webinar: Webinar;
 };
 const RenderWebinar = (props: Props) => {
+  const router = useRouter();
+  const pathname = usePathname();
+  const { attendee } = useAttendeeStore();
   const { apiKey, token, callId, user, webinar, error } = props;
+
+  useEffect(() => {
+    if (error) {
+      toast.error(error);
+      router.push(pathname);
+    }
+  }, [error]);
   // TODO : build waiting room and live webinar
   return (
     <React.Fragment>
